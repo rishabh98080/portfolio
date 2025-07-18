c57ec790ec0f1929722c397ca0b3493d82b67554
rishabh98080 <rishabh24273239pandey@gmail.com>
2025-07-18 13:07:56 +0530

    Changes have been made
    Changes have been made..

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -5,15 +5,32 @@ function Projects() {
   return (
     <section id="projects">
       <ProjectCard
-        name="Awesome Portfolio Website"
-        link="https://your-project-demo-link.com"
+        name="Banking System"
+        link="https://github.com/rishabh98080/Banking-System-with-SQL-in-JAVA"
         bullets={[
-          'Built with React and Vite',
-          'Responsive and modern UI',
-          'Showcases my web development skills',
-          'Deployed on Netlify'
+          'Built using Java and MySQL',
+          'Demonstrate all the feature of banking service',
+          'Use JDBC',
         ]}
       />
+            <ProjectCard
+        name="Currency Converter"
+        link="https://github.com/rishabh98080/Currency-Converter-in-Java"
+        bullets={[
+          'Built using Java and JSON',
+          'Demostrate the usage of an API',
+          'Uses JSON from retrieval of real-time data',
+        ]}
+      />
+            <ProjectCard
+        name="Beginner in Web Development"
+        link="https://github.com/rishabh98080/Background-Changing-Colour-using-HTML-CSS-JavaScript"
+        bullets={[
+          'Built using HTML,JavaScript,CSS',
+          'This is Project inn which you can chnage color of background with buttons',
+        ]}
+      />
+      
     </section>
   );
 }
